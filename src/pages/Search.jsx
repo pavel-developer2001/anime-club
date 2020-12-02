99b1179406ea7d7aeb4712dc6ec1bf3a5b3fbe2f
pavel-developer2001 <gaidukpavel18@gmail.com>
2020-12-02 15:59:37 +0300

    add Search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,46 @@ import React from "react";
 
 const Search = () => {
     return (
-        <div className="search">search</div>
+        <div className="search">
+            <div className="search__back">
+                <svg className="search__back-icon" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 13L1 7L7 1" stroke="#F2F2F2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <span className="search__back-text">Назад</span>
+            </div>
+            <div className="search__content">
+                <div className="search__content-head">
+                    <svg  className="search__content-head-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 17C6.58172 17 3 13.4183 3 9C3 4.58172 6.58172 1 11 1C15.4183 1 19 4.58172 19 9C19 13.4183 15.4183 17 11 17Z" stroke="#EB752B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M1.0001 18.9999L5.3501 14.6499" stroke="#EB752B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <input type="text" placeholder="Поиск" className="search__content-head-input" />
+                    <svg className="search__content-head-input-icon"  width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 19V12" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M4 8V1" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 19V10" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 6V1" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M20 19V14" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M20 10V1" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M1 12H7" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M9 6H15" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M17 14H23" stroke="#EB752B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+                <div className="search__content-tags">
+                    <div className="search__content-tags-tag">
+                        <span className="search__content-tags-tag-item">#Аниме</span>
+                        <span className="search__content-tags-tag-item">#Манга</span>
+                        <span className="search__content-tags-tag-item">#Музыка</span>
+                        <span className="search__content-tags-tag-item">#Манхва</span>
+                        <span className="search__content-tags-tag-item">#Маньхуа</span>
+                        <span className="search__content-tags-tag-item">#Комиксы</span>
+                        <span className="search__content-tags-tag-item">#Топ5</span>
+                    </div>
+                    <span className="search__content-tags-tag-count">+ 105</span>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Search;
